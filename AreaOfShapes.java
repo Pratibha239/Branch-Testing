0355e518cbd52b35a14f1ee79a06e858f514d517
Pratibha <pratibhapc239@gmail.com>
2022-12-05 15:25:13 +0530

    added rhombus area

--- AreaOfShapes.java
+++ AreaOfShapes.java
@@ -16,6 +16,7 @@ import java.util.Scanner;
           System.out.println("2: Area of Square");
          System.out.println("3: Area of Rectangle");
          System.out.println("4: Area of Circle");
+         System.out.println("5: Area of Rhombus");
     System.out.print("Make your choice: ");  
              ch = sc.nextInt(); // reading user's choice
               switch (ch)
@@ -50,6 +51,15 @@ import java.util.Scanner;
                  radius = sc.nextInt();
                  ar = radius * radius * 22 / 7;
                  System.out.println("Area of the Circle is " + ar + "\n\n");
+                 break;
+
+                 case 5:
+                 System.out.print("Enter the first diagonal of a Rhombus \n");
+                 diagonal1 = sc.nextInt();
+                 System.out.print("Enter the second diagonal of the Rhombus \n");
+                 diagonal2 = sc.nextInt();
+                 ar = diagonal1 * diagonal2 * 1 / 2;
+                 System.out.println("Area of the Rhombus is " + ar + "\n\n");
                  break;
         
                 default:
