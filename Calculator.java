6d22ef2bc9a8410f7314d7d5080cfd7f83fb0dc3
Pratibha <pratibhapc239@gmail.com>
2022-12-05 15:27:25 +0530

    added division

--- Calculator.java
+++ Calculator.java
@@ -39,6 +39,12 @@ class Calculator {
         System.out.println(number1 + " * " + number2 + " = " + result);
         break;
 
+      // performs division between numbers
+      case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;
+
       
 
       default:
